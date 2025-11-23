6e0f03783cb0304b7adf8c9566029ac7c995ccf4
Alex He <heye_dev@163.com>
2025-11-23 17:03:43 +0800

    Update model list
    Signed-off-by: Alex He <heye_dev@163.com>

--- service.sh
+++ service.sh
@@ -1,10 +1,12 @@
-export PATH=$PATH:${HOME}/llama.cpp/build/bin/
+export PATH=$PATH:${HOME}/llamacpp-vulkan-b7132/build/bin/
 
-export MODEL="${HOME}/GGUF/Qwen2.5-VL-3B-Instruct-GGUF/Qwen2.5-VL-3B-Instruct-Q8_0.gguf"
-export MMPROJ="${HOME}/GGUF/Qwen2.5-VL-3B-Instruct-GGUF/mmproj-Qwen2.5-VL-3B-Instruct-Q8_0.gguf"
+#export HSA_OVERRIDE_GFX_VERSION=11.0.0
 
-#export MODEL="${PWD}/SmolVLM2-500M-Video-Instruct-GGUF/SmolVLM2-500M-Video-Instruct-Q8_0.gguf"
-#export MMPROJ="${PWD}/SmolVLM2-500M-Video-Instruct-GGUF/mmproj-SmolVLM2-500M-Video-Instruct-Q8_0.gguf"
+#export MODEL="${HOME}/GGUF/Qwen3VL-8B-Thinking-GGUF/Qwen3VL-8B-Thinking-Q4_K_M.gguf"
+#export MMPROJ="${HOME}/GGUF/Qwen3VL-8B-Thinking-GGUF/mmproj-Qwen3VL-8B-Thinking-Q8_0.gguf"
+
+export MODEL="${HOME}/GGUF/SmolVLM2-500M-Video-Instruct-GGUF/SmolVLM2-500M-Video-Instruct-Q8_0.gguf"
+export MMPROJ="${HOME}/GGUF/SmolVLM2-500M-Video-Instruct-GGUF/mmproj-SmolVLM2-500M-Video-Instruct-Q8_0.gguf"
 
 # VLM Service for AMD ROCm™
 # This script starts the llama-server with the Qwen2.5-VL-3B-Instruct model
@@ -15,7 +17,8 @@ export MMPROJ="${HOME}/GGUF/Qwen2.5-VL-3B-Instruct-GGUF/mmproj-Qwen2.5-VL-3B-Ins
 #
 # The service will start on port 8080 and can be accessed by demo.html
 # or any client making requests to the /v1/chat/completions endpoint.
-HSA_OVERRIDE_GFX_VERSION=11.0.0 llama-server \
+
+llama-server \
 	-m ${MODEL} \
 	--mmproj ${MMPROJ} \
 	--host 0.0.0.0 \
